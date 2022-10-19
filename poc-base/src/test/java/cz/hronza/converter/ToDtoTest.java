bf8731e9b049ae0f7fadddeae85b1defd69985b1
Roman Hronza <roman.hronza@gmail.com>
2022-10-19 18:06:13 +0200

    add convertJson test

--- poc-base/src/test/java/cz/hronza/converter/ToDtoTest.java
+++ poc-base/src/test/java/cz/hronza/converter/ToDtoTest.java
@@ -1,7 +1,11 @@
 package cz.hronza.converter;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import cz.hronza.common.Person;
 import cz.hronza.common.PersonDto;
+import cz.hronza.utils.JsonConverter;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -58,5 +62,23 @@ class ToDtoTest {
         assertEquals(HRONZOVAA, personDtoRow2.getName().get(1));
     }
 
+    @Test
+    void jsonConvertToObject() throws JsonProcessingException {
 
+        String jsonFromObject = JsonConverter.toJsonFromObject(people);
+
+        JsonNode jsonNode = new ObjectMapper().readTree(jsonFromObject);
+
+        assertEquals(2,jsonNode.size());
+
+        JsonNode jsonNodeRow1 = jsonNode.get(0);
+        assertEquals(ROMAN, jsonNodeRow1.get("firstName").asText());
+        assertEquals(HRONZA, jsonNodeRow1.get("surname").asText());
+        assertEquals(333, jsonNodeRow1.get("age").asInt());
+
+        JsonNode jsonNodeRow2 = jsonNode.get(1);
+        assertEquals(MONIKA, jsonNodeRow2.get("firstName").asText());
+        assertEquals(HRONZOVAA, jsonNodeRow2.get("surname").asText());
+        assertEquals(18, jsonNodeRow2.get("age").asInt());
+    }
 }
