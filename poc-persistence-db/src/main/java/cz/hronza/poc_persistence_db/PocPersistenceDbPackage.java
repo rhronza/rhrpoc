385b5c258145d85fa674f238ede2b8535e535e53
Roman Hronza <roman.hronza@gmail.com>
2022-11-07 14:25:55 +0100

    add poc-persistence-db module

---
+++ poc-persistence-db/src/main/java/cz/hronza/poc_persistence_db/PocPersistenceDbPackage.java
@@ -0,0 +1,8 @@
+package cz.hronza.poc_persistence_db;
+/**
+ * The sole purpose of this class is represent its package in order to allow for a type-safe component scanning.
+ */
+public class PocPersistenceDbPackage {
+    private PocPersistenceDbPackage() {
+    }
+}
