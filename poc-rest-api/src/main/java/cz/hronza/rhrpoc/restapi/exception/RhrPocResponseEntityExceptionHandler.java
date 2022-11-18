7911e9cec35f2ec966fe8f1f0c3bccc2d361207c
Roman Hronza <roman.hronza@gmail.com>
2022-11-18 15:22:08 +0100

    throw RhrCannotBeDividedByZero and add tests

---
+++ poc-rest-api/src/main/java/cz/hronza/rhrpoc/restapi/exception/RhrPocResponseEntityExceptionHandler.java
@@ -0,0 +1,8 @@
+package cz.hronza.rhrpoc.restapi.exception;
+
+import cz.hronza.rhrpoc.core.common.exception.RhrPocExceptionHandler;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class RhrPocResponseEntityExceptionHandler extends RhrPocExceptionHandler {
+}
