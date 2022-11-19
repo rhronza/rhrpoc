78267be4af50049d51c1fc6f722873a708d3734a
Roman Hronza <roman.hronza@gmail.com>
2022-11-19 12:06:26 +0100

    add configuration to test in "poc-rest-api module"

---
+++ poc-rest-api/src/test/java/config/RhrPocRestApiTestConfiguration.java
@@ -0,0 +1,9 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class RhrPocRestApiTestConfiguration implements WebMvcConfigurer {
+    
+}
