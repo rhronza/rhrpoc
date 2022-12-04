0212c89ce3152a5637df29914551417cc76715f3
Roman Hronza <roman.hronza@gmail.com>
2022-12-04 13:12:05 +0100

    add basic auth

--- poc-rest-api/src/main/java/cz/hronza/rhrpoc/restapi/config/PocRestApiConfiguration.java
+++ poc-rest-api/src/main/java/cz/hronza/rhrpoc/restapi/config/PocRestApiConfiguration.java
@@ -1,6 +1,7 @@
 package cz.hronza.rhrpoc.restapi.config;
 
 import cz.hronza.rhrpoc.business_logic.config.PocBusinessLogicConfiguration;
+import cz.hronza.rhrpoc.core.api.configuration.RhrpocCoreApiConfiguration;
 import cz.hronza.rhrpoc.restapi.PocRestApiPackage;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +9,7 @@ import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackageClasses = PocRestApiPackage.class)
-@Import(value = PocBusinessLogicConfiguration.class )
+@Import(value = {PocBusinessLogicConfiguration.class, RhrpocCoreApiConfiguration.class} )
 public class PocRestApiConfiguration {
     
 }
