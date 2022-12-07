c69883061733e94c844f5d716cc473f90a8d5ca2
Roman Hronza <roman.hronza@gmail.com>
2022-12-07 19:29:47 +0100

    add secure call easy-be including generate x-corelation-id

--- poc-easy-be/src/main/java/cz/hronza/rhpoc/easy_be/service/EasyBeClientImpl.java
+++ poc-easy-be/src/main/java/cz/hronza/rhpoc/easy_be/service/EasyBeClientImpl.java
@@ -1,6 +1,9 @@
 package cz.hronza.rhpoc.easy_be.service;
 
 import cz.hronza.rhrpoc.core.api.dto.OutputDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
@@ -14,17 +17,20 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 import static cz.hronza.rhpoc.easy_be.config.PocEasyBeConfiguration.EASY_BE_REST_TEMPLATE_NAME;
 
 @Service
 public class EasyBeClientImpl implements EasyBeClient {
 
-    private RestTemplate restTemplate;
+    private final RestTemplate restTemplate;
     public final String hostName;
     public final String token;
 
-    private String END_POINT_NAME = "/reverse-endpoint";
+    private static final String END_POINT_NAME = "/reverse-endpoint";
+
+    private final Logger log = LoggerFactory.getLogger(EasyBeClientImpl.class);
 
 
     public EasyBeClientImpl(
@@ -38,15 +44,14 @@ public class EasyBeClientImpl implements EasyBeClient {
 
     @Override
     public ResponseEntity<OutputDto> getReverseEndpoint(String id, String name) {
-        ResponseEntity<OutputDto> outputDtoResponseEntity = restTemplate
+        return restTemplate
                 .exchange(initUrlHost(id, name),
                         HttpMethod.GET,
                         new HttpEntity<>("body", initHttpHeaders()),
                         new ParameterizedTypeReference<OutputDto>() {
                         });
-        return outputDtoResponseEntity;
-    }
 
+    }
 
     private URI initUrlHost(String id, String name) {
         return UriComponentsBuilder
@@ -61,14 +66,18 @@ public class EasyBeClientImpl implements EasyBeClient {
     private HttpHeaders initHttpHeaders() {
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
-        // TODO dodělat na straně easy-be
-//        httpHeaders.set("Authorization", String.format("Bearer %s", token));
+        httpHeaders.set("authorization", String.format("Bearer %s", token));
 
-        //TODO dodělat někde
-//        httpHeaders.add("X-Correlation-Id", MDC.get("correlationId"));
+        String correlationId = MDC.get("correlationId"); // tohle nic nevrací!!
+        correlationId = getRandomUUID(); // takže si to beru random UUID
 
-
+        log.info("correlationId={}", correlationId);
+        httpHeaders.add("X-Correlation-Id", correlationId);
         return httpHeaders;
 
+    }
+
+    private String getRandomUUID() {
+        return UUID.randomUUID().toString();
     }
 }
